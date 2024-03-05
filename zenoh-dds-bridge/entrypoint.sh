a86c956f4e0a4b4a0176a42c8c115ee465d01052
Kalin Ivanov <ivanovkalin7@gmail.com>
2024-03-05 15:45:31 +0000

    hardcoded ip, works

--- zenoh-dds-bridge/entrypoint.sh
+++ zenoh-dds-bridge/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/ash
 echo " * Starting: /zenoh-bridge-ros2dds $*"
-exec /zenoh-bridge-ros2dds --connect tcp/talker-host:7447 $*
+exec /zenoh-bridge-ros2dds --connect tcp/[fc94:cf6d:d525:d120:1b6f:6c0a:8d8d:1050]:7447 $*
