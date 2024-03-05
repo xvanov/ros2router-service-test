fd6c512d1e347375187789130512595157c88875
Kalin Ivanov <ivanovkalin7@gmail.com>
2024-03-05 21:49:14 +0000

    dds-bridge with husarnet

--- zenoh-dds-bridge/entrypoint.sh
+++ zenoh-dds-bridge/entrypoint.sh
@@ -1,3 +1,9 @@
-#!/bin/ash
-echo " * Starting: /zenoh-bridge-ros2dds $*"
-exec /zenoh-bridge-ros2dds --connect tcp/[fc94:cf6d:d525:d120:1b6f:6c0a:8d8d:1050]:7447 $*
+#!/bin/bash
+# have to run husarnet with sudo
+husarnet-daemon &
+sleep 10
+husarnet join ${JOINCODE} listener-host
+sleep 10
+ROS_DISTRO=humble zenoh-bridge-ros2dds --connect tcp/talker-host:7447 &
+exec bash
+
