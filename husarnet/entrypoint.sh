a3a040885e14b40957ce2c03dfb48c56d1d5e682
Kalin Ivanov <kalin@botbuilt.com>
2024-03-07 14:03:36 -0500

    clean up org

--- husarnet/entrypoint.sh
+++ husarnet/entrypoint.sh
@@ -3,6 +3,6 @@
 husarnet-daemon &
 sleep 10
 husarnet join ${JOINCODE} listener-host
-ROS_DISTRO=humble zenoh-bridge-ros2dds --connect tcp/talker-host:7447 &
+ROS_DISTRO=humble zenoh-bridge-ros2dds --connect tcp/talker-host:7447 --config /listener-dds-config.json5&
 exec bash
 
